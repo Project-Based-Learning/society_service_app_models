4687c9d9ddad3553e1929b9628da28a395715c73
Rosemary18 <zulkarnain.ahmad18@gmail.com>
2021-06-17 22:20:33 +0700

    breaking change

--- models/submissions/SubmissionIdentityGroupModel.js
+++ models/submissions/SubmissionIdentityGroupModel.js
@@ -29,7 +29,7 @@ const SUBMISSION = new Schema({
                     type: String,
                     default: ""
                 },
-                identityGroupid: {
+                identityGroupId: {
                     type: String,
                     default: ""
                 },
